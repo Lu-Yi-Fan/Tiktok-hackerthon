c9fa21def8997ea562c0007a358d9545a24b2148
summerthia <summerthia12@gmail.com>
2023-09-03 23:46:43 +0800

    test

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import pandas as pd
 ads_dimension = pd.read_excel("Dataset.xlsx",sheet_name='ads dimension (dim table)',header=1)
 moderator = pd.read_excel("Dataset.xlsx",sheet_name='moderator dimension (dim table)',header=0)
 
+#test
