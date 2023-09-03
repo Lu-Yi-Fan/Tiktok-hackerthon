6af1b6c6b4b2336227f9174d385f1a16a7ec968d
lu-yi-fan <88626655+Lu-Yi-Fan@users.noreply.github.com>
2023-09-03 23:15:22 +0800

    created repo

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+ads_dimension = pd.read_excel("Dataset.xlsx",sheet_name='ads dimension (dim table)',header=1)
+moderator = pd.read_excel("Dataset.xlsx",sheet_name='moderator dimension (dim table)',header=0)
+
